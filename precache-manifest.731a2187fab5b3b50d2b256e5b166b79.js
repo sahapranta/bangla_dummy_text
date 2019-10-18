5395ac70e6558e3fb760439f590cc02577a5d043
sahapranta <pranta@gmail.com>
2019-10-18 11:20:28 +0600

    Updates

---
+++ precache-manifest.731a2187fab5b3b50d2b256e5b166b79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5691f2857312807f9adbfd1d13f335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643e92b900c594396ed0",
+    "url": "/static/css/main.8fc703ca.chunk.css"
+  },
+  {
+    "revision": "42857aa348dac1089a14",
+    "url": "/static/js/2.41361fd3.chunk.js"
+  },
+  {
+    "revision": "643e92b900c594396ed0",
+    "url": "/static/js/main.04597799.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c6351c0b46f41c88d6b18fe1e203fed",
+    "url": "/static/media/Mukti.0c6351c0.ttf"
+  },
+  {
+    "revision": "7a3c71b41ba2095776dd87b1aebd7e8b",
+    "url": "/static/media/Mukti.7a3c71b4.woff"
+  },
+  {
+    "revision": "fe9a859b24c38f486d7b350967e6cf66",
+    "url": "/static/media/Mukti.fe9a859b.eot"
+  },
+  {
+    "revision": "66276433514268dcc7875255d7c787fc",
+    "url": "/static/media/file.66276433.svg"
+  }
+]);
